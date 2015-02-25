5c2db9147810e131e011f3152c78e627e67e63f8
spcmastertim <tim.quinn@playonsports.com>
2015-02-25 16:26:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TqTest::Application.config.secret_key_base = '67f9ec4f150ffc25b26bcc37ee4c37a11498addb26c5b8d62aebc4c7f41c11cb540dce2824a9cde5fc21458f2564debbf1baea44f249d067bd44109193e57c03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TqTest::Application.config.session_store :cookie_store, key: '_tq_test_session'
